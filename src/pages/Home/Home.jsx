e35c38452e5805649cdbcab9e42cbc16de612e6f
V. Deepak <deepakshakthi2003@gmail.com>
2025-05-21 09:04:18 +0530

    Fix: correct import paths in Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
-import AppDownload from '../../components/AppDownload/AppDOwnload'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 
 const Home = () => {
